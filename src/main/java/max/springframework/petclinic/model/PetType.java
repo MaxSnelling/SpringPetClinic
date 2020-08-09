ff04758deabedd7c1b7f77538e29fbb49ee37e6e
maxsnelling <max.snelling@outlook.com>
2020-08-09 14:41:02 +0100

    Added basic entity POJO's

---
+++ src/main/java/max/springframework/petclinic/model/PetType.java
@@ -0,0 +1,14 @@
+package max.springframework.petclinic.model;
+
+public class PetType {
+
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
---
+++ src/main/java/max/springframework/petclinic/model/Vet.java
@@ -0,0 +1,4 @@
+package max.springframework.petclinic.model;
+
+public class Vet extends Person {
+}
