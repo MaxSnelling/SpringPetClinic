8436af852acbd7729f9c83a53149bdf5010ffbd3
maxsnelling <max.snelling@outlook.com>
2020-08-16 14:04:41 +0100

    Added automatic ID generation in map based service

--- pet-clinic-web/src/main/java/max/springframework/petclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/max/springframework/petclinic/bootstrap/DataLoader.java
@@ -22,25 +22,21 @@ public class DataLoader implements CommandLineRunner {
     public void run(String... args) throws Exception {
 
         Owner owner1 = new Owner();
-        owner1.setId(1L);
         owner1.setFirstName("John");
         owner1.setLastName("Smith");
         ownerService.save(owner1);
 
         Owner owner2 = new Owner();
-        owner2.setId(2L);
         owner2.setFirstName("Mary");
         owner2.setLastName("Collins");
         ownerService.save(owner2);
 
         Vet vet1 = new Vet();
-        vet1.setId(1L);
         vet1.setFirstName("James");
         vet1.setLastName("Black");
         vetService.save(vet1);
 
         Vet vet2 = new Vet();
-        vet2.setId(2L);
         vet2.setFirstName("Mark");
         vet2.setLastName("Daniels");
         vetService.save(vet2);
